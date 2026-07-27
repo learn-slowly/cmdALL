f0bdc6600d42fe52dfb26a8f1e16ee2e376ece0b
레고 <redoutk@gmail.com>
2026-07-28 08:12:25 +0900

    빌드: 버전 0.9.416 (검색창 정렬·본문 검색 버그 수정 반영)

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -204,7 +204,7 @@ cat > "$PLIST" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.9.415</string>
+  <string>0.9.416</string>
   <key>CFBundleURLTypes</key>
   <array>
     <dict>
