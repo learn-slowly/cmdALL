b54810e24be1c955280200e59e7e20f39aca94f1
레고 <redoutk@gmail.com>
2026-07-27 09:57:22 +0900

    빌드: 버전 0.9.413 (챗GPT 로그인 추가)

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -204,7 +204,7 @@ cat > "$PLIST" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.9.412</string>
+  <string>0.9.413</string>
   <key>CFBundleURLTypes</key>
   <array>
     <dict>
