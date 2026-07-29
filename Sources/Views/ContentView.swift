434a6699155ce09b81c072c671f87b941404b40e
레고 <redoutk@gmail.com>
2026-07-29 23:43:25 +0900

    빌드: 버전 0.9.418 (사진 속 글자 검색 이미지 OCR 포함)

--- Sources/Views/ContentView.swift
+++ Sources/Views/ContentView.swift
@@ -496,7 +496,7 @@ struct WindowAccessor: NSViewRepresentable {
 
 enum AppInfo {
     static var version: String {
-        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.9.417"
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.9.418"
     }
     static var build: String {
         Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -204,7 +204,7 @@ cat > "$PLIST" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.9.417</string>
+  <string>0.9.418</string>
   <key>CFBundleURLTypes</key>
   <array>
     <dict>
