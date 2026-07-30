66f8bd6b16179b00836062c975fa47ed21ec3306
레고 <redoutk@gmail.com>
2026-07-30 15:29:25 +0900

    빌드: 버전 0.9.420 (문서 정합성 + 방향 결정 3건 + 듀얼 페인 미디어 재생 + AppState 13개 파일 분리)

--- Sources/Views/ContentView.swift
+++ Sources/Views/ContentView.swift
@@ -500,7 +500,7 @@ struct WindowAccessor: NSViewRepresentable {
 
 enum AppInfo {
     static var version: String {
-        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.9.419"
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.9.420"
     }
     static var build: String {
         Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -204,7 +204,7 @@ cat > "$PLIST" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.9.419</string>
+  <string>0.9.420</string>
   <key>CFBundleURLTypes</key>
   <array>
     <dict>
